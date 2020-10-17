2c3e20c08f865b4a3ac5f601efe2c2c6b2af7f09
Shiwei Huang <shiwei.julia.huang@gmail.com>
2020-10-16 23:17:40 -0400

    add additional routes

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
-
+from data import Resources
 app = Flask(__name__)
 
+Resources = Resources()
 
 @app.route('/')# add app route
 def index():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/resources')
+def resources():
+    return render_template('resources.html', resources = Resources)    
+
+
+@app.route('/resource/<string:id>')
+def resource(id):
+    return render_template('resource.html', id=id)    
 
 if __name__ == '__main__':
     app.run(debug = True)
